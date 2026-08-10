3b1a77fba568d043135084189bd9c3a8e027ed0a
Scott Hsiao <scott.hsiao@sunbirddcim.com>
2026-08-10 16:00:55 +0800

    [Step 3-5: Extract Class from Private Method] Extract TaskList::error to Error class

--- src/main/java/com/codurance/training/tasks/TaskList.java
+++ src/main/java/com/codurance/training/tasks/TaskList.java
@@ -7,10 +7,8 @@ import java.io.PrintWriter;
 
 import com.codurance.training.tasks.entity.ToDoList;
 import com.codurance.training.tasks.entity.ToDoListId;
-import com.codurance.training.tasks.entity.Project;
-import com.codurance.training.tasks.entity.Task;
-import com.codurance.training.tasks.entity.TaskId;
 import com.codurance.training.tasks.usecase.Add;
+import com.codurance.training.tasks.usecase.Error;
 import com.codurance.training.tasks.usecase.Help;
 import com.codurance.training.tasks.usecase.SetDone;
 import com.codurance.training.tasks.usecase.Show;
@@ -71,7 +69,7 @@ public final class TaskList implements Runnable {
                 new Help(out).help();
                 break;
             default:
-                error(command);
+                new Error(out).error(command);
                 break;
         }
     }
@@ -82,21 +80,6 @@ public final class TaskList implements Runnable {
 
     private void uncheck(String idString) {
         new SetDone(toDoList, out).setDone(idString, false);
-    }
-
-    private void help() {
-        out.println("Commands:");
-        out.println("  show");
-        out.println("  add project <project name>");
-        out.println("  add task <project name> <task description>");
-        out.println("  check <task ID>");
-        out.println("  uncheck <task ID>");
-        out.println();
-    }
-
-    private void error(String command) {
-        out.printf("I don't know what the command \"%s\" is.", command);
-        out.println();
     }
 
 }
